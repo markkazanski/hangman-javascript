4faa7f579c6a638655d8d007bf29dc638473c254
Mark Kazanski <markkazanski@gmail.com>
2017-10-10 21:02:32 -0500

    progress

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,108 @@
+/*
+choose random word
+    display number of blanks
+
+capture user input
+
+    Record guessed letter.
+
+    check if guess correct. 
+        Correct
+            Reveal letter
+
+        Wrong
+            Decrement guesses left
+            Add to hangman
+
+        Check if won.
+        Check if lost.
+
+*/
+
+var wordList = ["cat", "dog", "ape", "monkey", "bird", "antelope"];
+var wordToGuess = "";
+var userGuess;
+var guessesLeft;
+var guessedLetters = [];
+
+//FUNCTIONS
+function initialize(){
+    guessesLeft = 7;
+    guessedLetters = [];
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+}
+
+function randomWord(){
+    wordToGuess = wordList[Math.floor(Math.random()*((wordList.length-1)-1+1)+1)];
+}
+
+function captureUserInput(){
+    document.onkeyup = function(event){ 
+        console.log(event.key); 
+        userGuess = event.key; 
+        recordGuesses(); //everytime key is pressed record the guess
+    }
+}
+
+function isGuessCorrect(){
+    var correct;
+
+    if( wordToGuess.indexOf(userGuess) == -1 ){ //WRONG
+        correct = false;
+    }else{ //CORRECT
+        correct = true;
+    }
+    return correct;
+}
+
+function revealLetter(){
+    console.log("RIGHt");
+}
+
+function wrongGuess(){
+    console.log("WRONG");
+    guessesLeft--;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    //Add to hangman
+}
+
+function createBlanks(){
+    var theWordBlank;
+    for(var i=0; i<wordToGuess.length; i++){
+        theWordBlank = document.getElementById("word");
+        theWordBlank.innerHTML += " _ ";
+        console.log(theWordBlank.innerHTML);
+    } 
+}
+
+function recordGuesses(){
+    if(isLetter(userGuess)){
+        guessedLetters.push(userGuess);
+    }
+    document.getElementById("guessed").innerHTML = guessedLetters;
+    console.log("recordGuesses " + guessedLetters);
+}
+
+function isLetter( string ){
+    return (/[a-zA-Z]/.test(string) && string.length === 1 );
+}
+//END FUNCTIONS
+
+//Function Calls
+window.onload = function(){
+
+    initialize();
+    randomWord();
+    createBlanks();
+
+    captureUserInput();
+
+    if(userGuess !== undefined){
+        if( isGuessCorrect() ){
+            revealLetter();
+        } else{
+            wrongGuess();
+        }
+    }
+
+};
