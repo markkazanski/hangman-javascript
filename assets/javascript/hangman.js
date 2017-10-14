776e0fac7e7e9ce79d0ebdbddec621451db12df7
Mark Kazanski <markkazanski@gmail.com>
2017-10-14 12:11:52 -0500

    changes

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -24,12 +24,23 @@ var wordToGuess = "";
 var userGuess;
 var guessesLeft;
 var guessedLetters = [];
+var letterReveal = []; //shows blanks and revealed letters
+var correctIndex = [];
 
 //FUNCTIONS
 function initialize(){
+    console.log("INITIALIZE");
     guessesLeft = 7;
     guessedLetters = [];
+    letterReveal = [];
+    correctIndex = [];
     document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("guessed").innerHTML = "";
+    document.getElementById("notification").innerHTML = "";
+    document.getElementById("play-again").style.display = "none";
+
+    randomWord();
+    createBlanks();
 }
 
 function randomWord(){
@@ -40,15 +51,52 @@ function captureUserInput(){
     document.onkeyup = function(event){ 
         console.log(event.key); 
         userGuess = event.key; 
-        recordGuesses(); //everytime key is pressed record the guess
+        
+        if(!isWinner()){ //Check if word revealed, won
+            if(!isGameOver()){ //Check if guesses left
+                if(userGuess !== undefined){ // is guess defined, is guess right/wrong
+                    if( isGuessCorrect2() ){
+                        revealLetter2();
+                    } else if( guessedBefore() === false ){
+                        if(isLetter(userGuess)) wrongGuess();
+                    }
 
-        if(userGuess !== undefined){ // is guess defined, is guess right/wrong
-            if( isGuessCorrect() ){
-                revealLetter();
-            } else{
-                wrongGuess();
+                    recordGuesses(); //everytime key is pressed record the guess
+                    if(isWinner())winner(); //check winning conditions again
+                }
+            }else{
+                console.log("GAME OVER");
+                document.getElementById("notification").innerHTML = "You Lose!";
+                document.getElementById("play-again").style.display = "block";
             }
+        }else{ //WINNER
+            console.log("WINNER");
+            winner();
         }
+    }
+}
+
+function winner(){
+    wins++;
+    document.getElementById("notification").innerHTML = "WINNER!";
+    document.getElementById("play-again").style.display = "block";
+}
+
+function isWinner(){
+    var winner = true;
+    for(var i=0; i<wordToGuess.length; i++ ){
+        if(letterReveal[i] !== wordToGuess[i]){
+            winner = false;
+        }
+    }
+    return winner;
+}
+
+function isGameOver(){
+    if(guessesLeft>0){
+        return false;
+    }else{
+        return true;
     }
 }
 
@@ -63,8 +111,35 @@ function isGuessCorrect(){
     return correct;
 }
 
+function isGuessCorrect2(){
+    var correct = false;
+    for(var i=0; i<wordToGuess.length; i++){
+        //console.log("wordToGuess[i] " + wordToGuess[i] + " userGuess: " + userGuess);
+        
+        if(wordToGuess[i] === userGuess){
+            correctIndex.push(i);
+            correct = true;
+        }
+    }
+    return correct;
+}
+
 function revealLetter(){
-    console.log("RIGHt");
+    console.log("RIGHT - Reveal Letter");
+    letterReveal[ wordToGuess.indexOf(userGuess) ] = userGuess;
+    console.log(letterReveal.toString());
+    document.getElementById("word").innerHTML = letterReveal;
+}
+
+function revealLetter2(){
+    for(var i=0; i < correctIndex.length; i++){
+       letterReveal[correctIndex[i]] = wordToGuess[correctIndex[i]]; 
+       console.log("letterreveal: " + letterReveal[correctIndex[i]] + "wordtoguess: " + wordToGuess[correctIndex[i]]);
+    }    
+
+    console.log("RIGHT - Reveal Letter");
+    console.log(letterReveal.toString());
+    document.getElementById("word").innerHTML = letterReveal;
 }
 
 function wrongGuess(){
@@ -76,11 +151,15 @@ function wrongGuess(){
 
 function createBlanks(){
     var theWordBlank;
+    document.getElementById("word").innerHTML = "";
     for(var i=0; i<wordToGuess.length; i++){
-        theWordBlank = document.getElementById("word");
-        theWordBlank.innerHTML += " _ ";
-        console.log(theWordBlank.innerHTML);
+        //theWordBlank = document.getElementById("word");
+        //theWordBlank.innerHTML += " _ ";
+        letterReveal[i] = " _ "
+        //console.log(theWordBlank.innerHTML);
+        console.log(letterReveal);
     } 
+    document.getElementById("word").innerHTML = letterReveal;
 }
 
 function recordGuesses(){
@@ -91,8 +170,9 @@ function recordGuesses(){
     console.log("recordGuesses " + guessedLetters);
 }
 
-function guessedBefore( guess ){
-    if(guessedLetters.indexOf(guess) === -1){
+function guessedBefore(){
+    console.log("Guessed Before" + guessedLetters.indexOf(userGuess));
+    if(guessedLetters.indexOf(userGuess) === -1){
         return false;
     }else{
         return true;
@@ -107,9 +187,12 @@ function isLetter( string ){
 //Function Calls
 window.onload = function(){
 
+    var resetButton = document.getElementById("play-again");
+    resetButton.onclick = initialize;
+
     initialize();
-    randomWord();
-    createBlanks();
+//    randomWord();
+//    createBlanks();
 
     captureUserInput();  
 
